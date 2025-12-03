3cf257ee6ea4e4dfcb2492e6890215ca2b5880d4
Carlos R Bello <ramirezbelloci@gmail.com>
2025-12-03 10:01:06 -0600

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Carlos Ismael Ramirez Bello on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NewsView()
+        }
+    }
+}
