82761cd7c8b50fbce738ba60864f8bd742fe6219
Carlos R Bello <ramirezbelloci@gmail.com>
2025-12-05 11:54:26 -0600

    feat: agregar archivo de configuración inicial

---
+++ News/Config.swift
@@ -0,0 +1,14 @@
+//
+//  Config.swift
+//  News
+//
+//  Created by Carlos Ismael Ramirez Bello on 05/12/25.
+//
+
+import Foundation
+
+enum Config {
+    static let newsAPIKey = "559dd9db1ff845ffaeeaacf050e3873b"
+    static let newsBaseURL = "https://newsapi.org/v2"
+    static let restCountriesURL = "https://restcountries.com/v3.1/all"
+}
