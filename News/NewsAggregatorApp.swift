58d6b3ca045cf46430bbeed0a2506e174445b9dc
Carlos R Bello <ramirezbelloci@gmail.com>
2025-12-05 11:51:41 -0600

    feat: agregar entry point NewsAggregatorApp con cuerpo mínimo

---
+++ News/NewsAggregatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAggregatorApp.swift
+//  News
+//
+//  Created by Carlos Ismael Ramirez Bello on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAggregatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Inicializando…")
+        }
+    }
+}
